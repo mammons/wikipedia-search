8ea8f6c59de8e08227fa28df5d14a6b9532048b2
Matt <ammons@gmail.com>
2017-03-08 14:16:04 -0500

    i don't know what i was doing

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
     $("form").submit(function (event) {
         var queryTerm = document.querySelector("#searchBox").value;
+        $('#searchBox').val('');
         event.preventDefault(); //this stops the page from refreshing
 
         $.getJSON('https://en.wikipedia.org/w/api.php?action=opensearch&datatype=json&limit=5&search=' + queryTerm + '&callback=?')
@@ -15,25 +16,8 @@ $(document).ready(function () {
                         "<h3>", data[1][i], "</h3></a>",
                         "<p class='subjectDescription'>", data[2][i], "</p>"
                     ].join("\n");
-                    console.log(appendHTML);
                     $('#results').append(appendHTML).hide().fadeIn("slow");
                 }
             });
     });
 });
-
-function populateDisplayWith(json) {
-    $("#results").empty();
-    console.log("populating");
-
-    for (var i = 0; i < json[1].length; i++) {
-        var appendHTML = ["<div class='result'>",
-            "<a href='" + json[3][i] + "' target = '_blank'>",
-            "<h3>" + json[1][i] + "</h3></a>",
-            "<p class='subjectDescription'>" + json[2][i] + "</p>"
-        ].join("\n");
-        //console.log(appendHTML);
-        $('#results').append(appendHTML).hide().fadeIn();
-    }
-
-}
